6a41c90c2b8b06982e7290555471428e7f1b81d0
hamzaberni <hamzaberni@gmail.com>
2023-09-15 20:45:04 +0200

    basic layout and js practice

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+console.log("i'm Berni");
+//setInterval(()=>console.log("hello"),1000);
+
+let x = 10;
+let y = 5;
+//console.log(x+y);
+x + y;
+let z = "mug";
+console.log(z);
+z = "cup";
+console.log(z);
